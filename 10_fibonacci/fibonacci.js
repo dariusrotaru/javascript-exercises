500d519f94f5f6686980c3bca8819b8c68db9528
Darius Rotaru <verd3d@gmail.com>
2024-12-03 13:00:02 +0200

    solved 10 fib

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,18 @@
-const fibonacci = function() {
-
+const fibonacci = function(a) {
+    a = Number(a);
+    if(a < 0){
+        return "OOPS";
+    } else if (a == 0) {
+        return 0;
+    }
+    let fibbNumLast = 0; 
+    let fibbNumNext = 1;
+    for(let i = 1; i < a; i++){
+        let temp = fibbNumNext; 
+        fibbNumNext += fibbNumLast;
+        fibbNumLast = temp;
+    }
+    return fibbNumNext;
 };
 
 // Do not edit below this line
